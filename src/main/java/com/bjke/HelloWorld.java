98cc075a282d6383ee347ba0c6711ababe59c6ab
bjke <kebingjun@zhuge.com>
2021-11-19 13:56:13 +0800

    添加spark

--- src/main/java/com/bjke/HelloWorld.java
+++ src/main/java/com/bjke/HelloWorld.java
@@ -1,8 +1,14 @@
 package com.bjke;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.sayHello("scala");
+        Logger logger = Logger.getLogger(HelloWorld.class);
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+        logger.fatal("fatal");
     }
 }

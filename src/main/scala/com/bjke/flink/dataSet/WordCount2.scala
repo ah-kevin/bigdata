384979622ed36eb1581bea70a76b47664b7d67ca
bjke <kebingjun@zhuge.com>
2021-11-30 10:12:43 +0800

    批 流

--- src/main/scala/com/bjke/flink/dataSet/WordCount2.scala
+++ src/main/scala/com/bjke/flink/dataSet/WordCount2.scala
@@ -1,25 +1,28 @@
 package com.bjke.flink.dataSet
 
 
+import org.apache.flink.api.common.RuntimeExecutionMode
 import org.apache.flink.streaming.api.scala._
 
 object WordCount2 {
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-    val lines: DataStream[String] = env.fromElements(
-      "Who's there?",
-      "I think I hear them. Stand, ho! Who's there?")
+//    env.setRuntimeMode(RuntimeExecutionMode.BATCH)
+//    env.setRuntimeMode(RuntimeExecutionMode.STREAMING)
+    env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC)
+    val lines: DataStream[String] = env.fromElements("itcast hadoop spark", "itcast hadoop spark", "itcast hadoop", "itcast")
 
     val count: DataStream[(String, Int)] = lines.flatMap {
       _.toLowerCase.split("\\W+") filter {
         _.nonEmpty
       }
-    }.map((_, 1))
+    }.map {
+      (_, 1)
+    }
       .keyBy(_._1)
       .sum(1)
     count.print()
     env.execute("wordCount")
-
   }
 
 }
